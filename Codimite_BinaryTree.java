4fb6213469ca8a8d012cd50f51d8f31bc3680f4d
AJA Saajith <ajsajith99@gmail.com>
2025-02-10 11:57:46 +0530

    first commit

---
+++ Codimite_BinaryTree.java
@@ -0,0 +1,18 @@
+
+import javax.swing.tree.TreeNode;
+
+public class Codimite_BinaryTree {
+
+    public static void findLeatNode(TreeNode root, String[] leafNodes, int index){
+        
+        if(root == null){
+            return;
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+
+    }
+    
+}
---
+++ TreeNode.java
@@ -0,0 +1,13 @@
+
+public class TreeNode {
+    public String value; 
+    public TreeNode left, right;
+ 
+     public TreeNode(String value) {
+         this.value = value;
+         this.left = this.right = null;
+     }
+     
+     
+ }
+ 
